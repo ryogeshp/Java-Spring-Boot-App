2c7f11d868b47acc6ed8db06f7da9b9e9e1a562f
ryogeshp <rjprajapati08@gmail.com>
2025-04-18 00:50:51 +0530

    Update StartApplication.java

--- Java-mvn-sonar-argo-k8s/java-spring-boot-app/src/main/java/com/yogeshp/StartApplication.java
+++ Java-mvn-sonar-argo-k8s/java-spring-boot-app/src/main/java/com/yogeshp/StartApplication.java
@@ -1,4 +1,4 @@
-package com.abhishek;
+package com.yogeshp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,8 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
+        model.addAttribute("title", "I have successfully built a spring boot application using Maven");
+        model.addAttribute("msg", "This application is deployed onto Kubernetes using Argo CD");
         return "index";
     }
 
